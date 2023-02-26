51a26666ac524ea8a57a4d6fa6683b7f78957c7f
Julien Prados <prados@CL-L9D3WNC2GY.local>
2023-02-26 14:31:29 +0100

    add binder

---
+++ .binder/install.R
@@ -0,0 +1,31 @@
+
+
+install.packages(c('Matrix','curl','XML','rjson','jsonlite','rhdf5','igraph','reshape2','Rcpp','devtools'))
+install.packages(c('LowRankQP','bmrm','limma'))
+install.packages(c('scales','RColorBrewer','wesanderson','scico'))
+install.packages(c('ggplot2','patchwork','ggrepel','ggraph','cowplot','ggnewscale'))
+install.packages(c('jpeg','png','ggpubr'))
+install.packages(c('irlba', 'umap', 'Rtsne'))
+install.packages(c('tidyverse','tidygraph'))
+install.packages(c('reticulate','Seurat'))
+install.packages(c('bs4Dash', 'reactlog', 'rentrez', 'httr2'))
+install.packages(c('gplots', 'readxl', 'plotly', 'rgl'))
+install.packages(c('rmarkdown','markdown','devtools'))
+
+
+install.packages(c('torch','luz'))
+torch::install_torch()
+devtools::install_github("AllenInstitute/cocoframer")
+
+
+# bioconductor packages
+BiocManager::install(c('ShortReads','Rsamtools','rtracklayer'))
+BiocManager::install(c('S4Vectors','IRanges','GenomicRanges','GenomicFeatures','GenomicAlignments','SummarizedExperiment','GenomicFiles'))
+BiocManager::install(c('Rhdf5lib','rhdf5','HDF5Array'))
+BiocManager::install(c('SingleCellExperiment','SpatialExperiment','LoomExperiment'))
+BiocManager::install(c('DropletUtils','scuttle','scran','scater','uwot','AUCell','SingleR','ComplexHeatmap'))
+BiocManager::install(c('biomaRt'))
+BiocManager::install(c('edgeR','DESeq2','GEOquery'))
+
+
+
