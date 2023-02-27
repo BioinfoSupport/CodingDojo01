67a4e3fb111feaf624297e6ccd1b59d2bb92de15
Julien Prados <prados@m2.home>
2023-02-27 13:24:14 +0100

    update R packages

--- .binder/install.R
+++ .binder/install.R
@@ -1,15 +1,17 @@
 
 
-install.packages(c('Matrix','curl','XML','rjson','jsonlite','rhdf5','igraph','reshape2','Rcpp','devtools'))
+
+install.packages(c('Matrix','igraph','reshape2','Rcpp','devtools'))
+install.packages(c('Matrix','curl','XML','rjson','jsonlite','readxl','rhdf5','igraph','reshape2','Rcpp','devtools'))
 install.packages(c('LowRankQP','bmrm','limma'))
 install.packages(c('scales','RColorBrewer','wesanderson','scico'))
-install.packages(c('ggplot2','patchwork','ggrepel','ggraph','cowplot','ggnewscale'))
+install.packages(c('ggplot2','patchwork','ggrepel','ggraph','ggnewscale'))
 install.packages(c('jpeg','png','ggpubr'))
 install.packages(c('irlba', 'umap', 'Rtsne'))
 install.packages(c('tidyverse','tidygraph'))
 install.packages(c('reticulate','Seurat'))
 install.packages(c('bs4Dash', 'reactlog', 'rentrez', 'httr2'))
-install.packages(c('gplots', 'readxl', 'plotly', 'rgl'))
+install.packages(c('gplots', 'plotly', 'rgl'))
 install.packages(c('rmarkdown','markdown'))
 
 
@@ -19,14 +21,15 @@ devtools::install_github("AllenInstitute/cocoframer")
 
 
 # bioconductor packages
-#install.packages(c('BiocManager'))
-#BiocManager::install(c('ShortReads','Rsamtools','rtracklayer'))
-#BiocManager::install(c('S4Vectors','IRanges','GenomicRanges','GenomicFeatures','GenomicAlignments','SummarizedExperiment','GenomicFiles'))
-#BiocManager::install(c('Rhdf5lib','rhdf5','HDF5Array'))
-#BiocManager::install(c('SingleCellExperiment','SpatialExperiment','LoomExperiment'))
-#BiocManager::install(c('DropletUtils','scuttle','scran','scater','uwot','AUCell','SingleR','ComplexHeatmap'))
-#BiocManager::install(c('biomaRt'))
-#BiocManager::install(c('edgeR','DESeq2','GEOquery'))
+install.packages("BiocManager")
+BiocManager::install(c('ShortReads','Rsamtools','rtracklayer'))
+BiocManager::install(c('S4Vectors','IRanges','GenomicRanges','GenomicFeatures','GenomicAlignments','SummarizedExperiment','GenomicFiles'))
+BiocManager::install(c('Rhdf5lib','rhdf5','HDF5Array'))
+BiocManager::install(c('edgeR','DESeq2'))
+BiocManager::install(c('SingleCellExperiment','scuttle','DropletUtils','scran','scater'))
+BiocManager::install(c('SpatialExperiment','LoomExperiment','uwot','AUCell','SingleR','ComplexHeatmap'))
+BiocManager::install(c('biomaRt','GEOquery'))
+
 
 
 
